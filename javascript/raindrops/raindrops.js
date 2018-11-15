214edfffffaf4ed4bc50dd145d71a8dea7c36430
Chachu <sharadjoe48@gmail.com>
2018-11-15 11:36:35 +0530

    completed the raindrop problem

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,17 +1,20 @@
 module.exports = class Raindrops {
     
     convert(number){
-        var out =""
-        if (number%3===0){
-            out += 'Pling'
+        var out ="",flag=0
+        if ((number%3)=== 0){
+            out += "Pling"
         }
-        if (number%5===0) {
-            out+='Plang'
+        if ((number%5)=== 0) {
+            out+="Plang"
         }
         if (number%7 === 0) {
-            out+='Plong'
+            out+="Plong"
         }
-        else{
+        
+        if(out.length>0){
+            return out
+        }else{
             return number.toString()
         }
 
