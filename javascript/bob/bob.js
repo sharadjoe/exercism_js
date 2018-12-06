9f1d9e942231d89b96a62c8d2db7e506f9c9ff35
Chachu <sharadjoe48@gmail.com>
2018-12-06 21:37:14 +0530

    finished bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,11 +1,27 @@
-/* eslint-disable no-unused-vars */
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
+const regex = /([a-z]|[0-9]|:)(?!.*\1)/g
 export const hey = (message) => {
-  //
-  // YOUR CODE GOES HERE
-  //
+  message = message.replace(/\s|\t/g, "")
+  if (message === '') {
+    return "Fine. Be that way!"
+  }
+  else if (message[message.length - 1] === '?' && message.match(regex) === null) {
+    return "Calm down, I know what I\'m doing!"
+  }
+  else if(message.match(regex) === null){
+    return "Whoa, chill out!"
+  } 
+  else if(message[message.length-1] === '?') {
+    return "Sure."
+  }
+  else if (message[message.length - 1] === '!' && (message.match(regex).length > 3)) {
+    return "Whatever."
+  }
+  else if (message[message.length - 1] === '!' && message.match(regex) !== null) {
+    return "Whoa, chill out!"
+  }
+  
+  else{
+    return "Whatever."
+  }
+  
 };
