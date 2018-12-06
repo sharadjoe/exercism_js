cbb1959c32416f6c9f2c85deedf6d9b02799f80a
Chachu <sharadjoe48@gmail.com>
2018-12-06 20:46:25 +0530

    isogram completed

---
+++ javascript/isogram/isogram.js
@@ -0,0 +1,15 @@
+const regex = /([a-z])(?!.*\1)/g
+export function isIsogram(string){
+    string = string.replace(/-/g, "")
+    string = string.replace(/\s/g, "")
+    string = string.toLowerCase()
+    if(string === ''){
+        return true
+    } else if (string.match(regex).length === string.length){
+            return true
+    }
+    else {
+        return false
+    }
+
+}
