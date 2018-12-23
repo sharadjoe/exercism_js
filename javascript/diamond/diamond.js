58ba9216baca9654887a8c12416d7c127ee1e7b7
Chachu <sharadjoe48@gmail.com>
2018-12-23 18:20:37 +0530

    redefining the  whole loops

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -1,22 +1,30 @@
-const final = []
+var final = []
 
-const alpha = "ABCDEFGHIJKLMNOPQRSTUVWIYZ"
+var alpha = "ABCDEFGHIJKLMNOPQRSTUVWIYZ"
 module.exports = class Diamond{
     constructor(char) {
         this.char = char
         this.result = this.makeDiamond()
+        return this.result
         
     }
 
     makeDiamond(){
-        var row = "",j=1
+        var row = "", j=1
         const index = (alpha.indexOf(this.char))+2
         const space_count = this.countSpace(index)
         while (j <= space_count) {
             row = row+ " "
             j += 1
         }
-        return row
+        for(var i=0;i<index;i++){
+            var mid = Math.floor(space_count/2)
+            if(i===0){
+                row[i] = alpha[i]
+            }
+            final.push(row)
+        }
+        return final.join('\n')
         
     }
 
