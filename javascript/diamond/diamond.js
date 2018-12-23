5ef97b430a287b21d02657b658944ea77c68a7df
Chachu <sharadjoe48@gmail.com>
2018-12-23 20:22:45 +0530

    made some further adjustments

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -6,18 +6,22 @@ module.exports = class Diamond{
             final = [],
             alpha = "ABCDEFGHIJKLMNOPQRSTUVWIYZ",
             index = (alpha.indexOf(char))+1,
-            space_count = this.countSpace(index)
+            space_count = this.countSpace(index),
+            first=0
         //return index
         var mid = Math.floor(space_count / 2)        
-        var count = space_count
         for(var i=0;i<index;i++){
+            var count = space_count
             var firstEntry = mid-j, lastEntry = mid+j
             while(count !== 0){
-                if(count===mid){
-                    row = row+alpha[k]
+                if(first===0 && row.length<space_count){
+                    if(count==mid || mid==0){row = row+alpha[k]
                     count-=1
-                    k+=1
-                } else if(count===firstEntry || count===lastEntry){
+                    k+=1}else {
+                        row = row+ " "
+                        count-=1
+                    }
+                } else if((count===firstEntry || count===lastEntry) && first!==0){
                     row = row+alpha[k]
                     count-=1
                     k+=1
