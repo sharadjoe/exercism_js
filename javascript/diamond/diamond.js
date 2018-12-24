dc38711cd2d9cfeeaffa9361ff2c80538ff57090
Chachu <sharadjoe48@gmail.com>
2018-12-24 21:13:35 +0530

    readded and modified

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -9,12 +9,32 @@ module.exports = class Diamond{
             space_count = this.countSpace(index),
             first=0
         //return index
+        //TODO:  ONE CHAR AND FIRST LINE FUNCTION
         var mid = Math.floor(space_count / 2)        
         for(var i=0;i<index;i++){
             var count = space_count
             var firstEntry = mid-j, lastEntry = mid+j
             while(count !== 0){
-                
+                if(mid==0){
+                    row = row+alpha[k]
+                }
+                else if(first===0){
+                    if(count==mid){row = row+alpha[k]
+                    count-=1
+                    first = 1
+                    k+=1}else {
+                        row = row+ " "
+                        count-=1
+                    }
+                } else if((count===firstEntry || count===lastEntry) && first!==0){
+                    row = row+alpha[k]
+                    count-=1
+                    k+=1
+                }else {
+                    row= row+ " "
+                    count-=1
+                }
+                j+=1
             }
             final.push(row)
         }
