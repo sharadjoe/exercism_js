d3e3bf4c194d399c744aa2f7369d4c989bf7005e
Chachu <sharadjoe48@gmail.com>
2018-12-26 10:08:36 +0530

    problem with var first

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -1,4 +1,4 @@
-var k=-1
+
 module.exports = class Diamond{
     makeDiamond(char){
         var row = "",
@@ -7,17 +7,18 @@ module.exports = class Diamond{
             alpha = "ABCDEFGHIJKLMNOPQRSTUVWIYZ",
             index = (alpha.indexOf(char))+1,
             space_count = this.countSpace(index),
-            first=0
+            first=0,
+            k=-1
 
         //return index
         //TODO:  ONE CHAR AND FIRST LINE FUNCTION
         k+=1
-        var mid = Math.floor(space_count / 2)        
+        var mid = (Math.floor(space_count / 2))+1        
         for(var i=0;i<index;i++){
             var count = space_count
             var firstEntry = mid-j, lastEntry = mid+j
             while(count !== 0){
-                if(mid === 0){
+                if(mid === 1){
                     row = row+alpha[k]
                     count-=1
                 }
@@ -25,6 +26,7 @@ module.exports = class Diamond{
                     if(count === mid){row = row+alpha[k]
                     count-=1
                     first = 1
+                    k+=1
                     }else {
                         row = row+ " "
                         count-=1
@@ -32,6 +34,8 @@ module.exports = class Diamond{
                 } else if((count === firstEntry || count === lastEntry) && first!==0){
                     row = row+alpha[k]
                     count-=1
+
+                    k+=1
                 
                 }else {
                     row= row+ " "
