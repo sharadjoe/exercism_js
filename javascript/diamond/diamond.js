194c3a1fb8acfbf8f24c9cdf488d3a59ec436273
Chachu <sharadjoe48@gmail.com>
2018-12-26 12:15:40 +0530

    complted the 2 cases

--- javascript/diamond/diamond.js
+++ javascript/diamond/diamond.js
@@ -2,20 +2,22 @@
 module.exports = class Diamond{
     makeDiamond(char){
         var row = "",
-            j = 1,
+            j = 0,
             final = [],
             alpha = "ABCDEFGHIJKLMNOPQRSTUVWIYZ",
             index = (alpha.indexOf(char))+1,
             space_count = this.countSpace(index),
             first=0,
-            k=0,iter=[]
+            k=0,
+            firstEntry,lastEntry
 
         //return index
         //TODO:  ONE CHAR AND FIRST LINE FUNCTION
         var mid = (Math.floor(space_count / 2))+1       
-        var firstEntry = mid - j, lastEntry = mid + j 
-        for(var i=0;i<index;i++){
+        for(var i=1;i<=index;i++){
             row=""
+            firstEntry = mid - j
+            lastEntry = mid + j
             var count = space_count
             while(count !== 0){
                 if(mid === 1){
@@ -34,25 +36,37 @@ module.exports = class Diamond{
                         }
                     }
                 } else if(first!==0){
-                    if ((count === firstEntry) || count === lastEntry){
+                    if ((count === (firstEntry)) || count === (lastEntry)){
                         row = row+alpha[k]
                         count-=1
-                    }else {
+                    }
+                    else {
                         row = row+" "
                         count-=1
+                        if (row.length === space_count) {
+                            k += 1
+                        }
                     }
                 
                 }else {
                     row= row+ " "
                     count-=1
                 }
-                j+=1
+                
             }
-            
+            j+=1
             final.push(row)
         }
-        final[final.length - 1]+="\n"
-        return final.join('\n')
+        if((i-1)===index){
+            var d=[]
+            for(j =(index-2);j>=0;j--){
+                final.push(final[j])
+                
+            }
+            final[final.length - 1] += "\n"
+            return final.join("\n")
+        }
+        
         
     }
 
