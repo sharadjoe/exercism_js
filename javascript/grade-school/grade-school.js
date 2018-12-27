1d544d4725a326e9dd931e7d60ad3be3be65f4ae
Chachu <sharadjoe48@gmail.com>
2018-12-27 17:14:54 +0530

    got the github sol

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,13 +1,24 @@
-function convert(obj){
-    return JSON.parse(JSON.stringify(obj))
+function clone(obj) {
+    return JSON.parse(JSON.stringify(obj));
 }
-var db
-module.exports = class School{
-    constructor(){
-        db={}
-    }
-    roster(){
-        return convert(db)
 
+let db;
+class School {
+    constructor() {
+        db = {};
+    }
+
+    add(student, level) {
+        db[level] = this.grade(level).concat(student).sort();
+    }
+
+    grade(level) {
+        return db[level] ? clone(db[level]).sort() : [];
+    }
+
+    roster() {
+        return clone(db);
     }
 }
+
+export default School;
