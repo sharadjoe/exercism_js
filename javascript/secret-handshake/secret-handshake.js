5e965d7c6b4d4d3a844dff6044f3a6693903d4a0
Chachu <sharadjoe48@gmail.com>
2018-12-27 16:02:12 +0530

    getting the binary conversion ready

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,16 +1,42 @@
 export const secretHandshake = (decimal) =>{
     var binary = toBinary(decimal)
+    var result = calList(binary)
     return binary
+    
 
 }
 
 function toBinary(decimal){
-    var str=[]
-    while(decimal===1){
-        str.push((decimal%2).toString())
+    var str="",val
+    while(decimal>0){
+        val = decimal%2
+        str+=val
         decimal = Math.floor(decimal/2)
     }
+    str = str.split("")
     str = str.reverse()
     str.join("")
-    return parseInt(str)
+    return str
+}
+
+function calList(binary){
+    var res = []
+    if(Math.floor(binary/1000)){
+        res.push('jump')
+        binary = binary%1000
+    }
+    if (Math.floor(binary / 100)) {
+        res.push('close your eyes')
+        binary = binary % 100
+    }
+    if (Math.floor(binary / 10)) {
+        res.push('double blink')
+        binary = binary % 10
+    }
+    if (Math.floor(binary / 1)) {
+        res.push('wink')
+        binary = binary % 1
+    }
+    return res
+
 }
--- javascript/secret-handshake/secret-handshake.spec.js
+++ javascript/secret-handshake/secret-handshake.spec.js
@@ -1,11 +1,11 @@
 import { secretHandshake } from './secret-handshake';
 
 describe('Secret Handshake', () => {
-  test('binary 1 (decimal 1) is a wink', () => {
+  xtest('binary 1 (decimal 1) is a wink', () => {
     expect(secretHandshake(1)).toEqual(['wink']);
   });
 
-  xtest('binary 10 (decimal 2) is a double blink', () => {
+  test('binary 10 (decimal 2) is a double blink', () => {
     expect(secretHandshake(2)).toEqual(['double blink']);
   });
 
