a5290942382e19d0138face938ae74051a28f67b
Chachu <sharadjoe48@gmail.com>
2019-01-08 23:07:36 +0530

    completed the problem

---
+++ javascript/anagram/anagram.js
@@ -0,0 +1,15 @@
+const normalize = str => str.toLowerCase().split('').sort().join();
+const sameWord = (word, candidate) => word.toLowerCase() === candidate.toLowerCase();
+const isAnagram = (word, candiate) => normalize(word) === normalize(candiate);
+
+export default class Anagram {
+    constructor(word) {
+        this.word = word;
+    }
+
+    matches(words) {
+        const wordsCopy = Array.isArray(words) ? words : [...words];
+        return wordsCopy.filter(candidate => !sameWord(this.word, candidate) &&
+            isAnagram(this.word, candidate));
+    }
+}
