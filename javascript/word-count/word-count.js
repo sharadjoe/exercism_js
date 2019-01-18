7315c5bf691660f5d6d841295d6eec050c72406c
Chachu <sharadjoe48@gmail.com>
2019-01-18 20:57:14 +0530

    got all the cases working

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,7 +1,7 @@
 module.exports = class Words {
     count(text){
         this.wordCount = {}
-        var splitted = text.split(" ")
+        var splitted = text.match(/\S+/g);
         splitted.forEach((element)=>{
             const wordLower = element.toLowerCase()
             this.wordCount[wordLower] = this.wordCount.hasOwnProperty(wordLower)? this.wordCount[wordLower]+1 : 1
