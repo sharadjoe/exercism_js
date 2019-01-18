fdd4c82b8a4f68e726c88833dc9dd8f00ec40efb
Chachu <sharadjoe48@gmail.com>
2019-01-18 21:35:01 +0530

    completed the solution

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,10 +1,9 @@
-export const encode = (text) =>{
-    if(text.length===0){
-        return ''
-    }else{
-        const justcount = text.split("").unique()
-        if(justcount.length === text.length){
-            return text
-        }
-    }
-}
+let run = /(\w)\1+/g;
+let compressed = /(\d+)(\w)/g;
+
+module.exports = {
+    encode: string =>
+        string.replace(run, match => match.length + match.charAt(0)),
+    decode: encoded =>
+        encoded.replace(compressed, (match, length, char) => char.repeat(length)),
+};
