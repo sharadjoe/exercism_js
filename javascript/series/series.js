58ca669b1bfcc24c9e3ddb0f6275358ab941f611
Chachu <sharadjoe48@gmail.com>
2019-01-20 09:07:31 +0530

    the digits part has been finished

---
+++ javascript/series/series.js
@@ -0,0 +1,13 @@
+module.exports = class Series {
+    constructor(props) {
+        this.number = props
+        this.digits = this.digits()
+    }
+    
+    digits(){
+        var digitslist = this.number.split("")
+        digitslist = digitslist.map(Number)
+        return digitslist
+
+    }
+}
