8be7fe921c15ce9a42b6732442ae6d973078d1c1
Chachu <sharadjoe48@gmail.com>
2019-01-30 21:46:13 +0530

    one more case to be fixed

--- javascript/bracket-push/bracket-push.js
+++ javascript/bracket-push/bracket-push.js
@@ -1,14 +1,33 @@
 export const bracketPush = (string) => {
     var divided = string.split(""),
-        i=0,
+        i=0,count=0,
         goodList=[],
         badList=[]
     while(i<divided.length){
-        if (divided[i] === '{' || divided[i] === '[' || divided[i] === '(' ) {
+        if ((divided[i] === '{' || divided[i] === '[' || divided[i] === '(') ) {
             goodList.push(divided[i])
-        }   else     {
-            badList.push(divided[i])
+        }else  if(goodList.length!==divided.length){
+            var oppo = opposite(goodList.pop()) || ""
+            if(oppo===divided[i]){
+                count+=1
+            }else{
+                return false
+            }   
         }
         i+=1
     }
+    if(count===divided.length/2){
+        return true
+    }
+}
+
+function opposite(char) {
+    if(char==="{"){
+        return "}"
+    } else if(char==="["){
+        return "]"
+    }else{
+        return ")"
+    }
+    
 }
--- javascript/bracket-push/bracket-push.spec.js
+++ javascript/bracket-push/bracket-push.spec.js
@@ -1,31 +1,31 @@
 import { bracketPush } from './bracket-push';
 
 describe('bracket push', () => {
-  xtest('checks for appropriate bracketing in a set of brackets', () => {
+  test('checks for appropriate bracketing in a set of brackets', () => {
     expect(bracketPush('{}')).toEqual(true);
   });
 
-  xtest('returns false for unclosed brackets', () => {
+  test('returns false for unclosed brackets', () => {
     expect(bracketPush('{{')).toEqual(false);
   });
 
-  xtest('returns false if brackets are out of order', () => {
+  test('returns false if brackets are out of order', () => {
     expect(bracketPush('}{')).toEqual(false);
   });
 
-  xtest('checks bracketing in more than one pair of brackets', () => {
+  test('checks bracketing in more than one pair of brackets', () => {
     expect(bracketPush('{}[]')).toEqual(true);
   });
 
-  xtest('checks bracketing in nested brackets', () => {
+  test('checks bracketing in nested brackets', () => {
     expect(bracketPush('{[]}')).toEqual(true);
   });
 
-  xtest('rejects brackets that are properly balanced but improperly nested', () => {
+  test('rejects brackets that are properly balanced but improperly nested', () => {
     expect(bracketPush('{[}]')).toEqual(false);
   });
 
-  xtest('checks bracket closure with deeper nesting', () => {
+  test('checks bracket closure with deeper nesting', () => {
     expect(bracketPush('{[)][]}')).toEqual(false);
   });
 
