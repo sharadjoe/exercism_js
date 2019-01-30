9053c0995d1298f42b7b1df15712577721ffbef2
Chachu <sharadjoe48@gmail.com>
2019-01-30 22:38:51 +0530

    completed the soluton

--- javascript/high-scores/high-scores.js
+++ javascript/high-scores/high-scores.js
@@ -1,8 +1,27 @@
-export class HighScores{
-    constructor(input){
-        this.input = input
-        this.scores = this.input
-        this.latest = this.input[this.input.length-1]
+export class HighScores {
+    constructor(scores) {
+        this.scores = scores;
+    }
 
+    get latest() {
+        return this.scores[this.scores.length - 1];
+    }
+
+    get highest() {
+        return Math.max(...this.scores);
+    }
+
+    get top() {
+        return [...this.scores]
+            .sort((a, b) => a - b)
+            .reverse()
+            .slice(0, 3);
+    }
+
+    get report() {
+        if (this.latest === this.highest) {
+            return `Your latest score was ${this.latest}. That's your personal best!`;
+        }
+        return `Your latest score was ${this.latest}. That's ${this.highest - this.latest} short of your personal best!`;
     }
 }
