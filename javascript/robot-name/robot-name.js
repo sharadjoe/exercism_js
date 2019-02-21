413c19d3cba8afb8dcda4b85af21b5856978f77f
Chachu <sharadjoe48@gmail.com>
2019-02-21 09:11:52 +0530

    completed the problem

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,28 @@
+const ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const BASE = 10;
+const usedNames = {};
+
+const random = max => Math.floor(Math.random() * max);
+
+const generateName = () => {
+    let name = ALPHA.charAt(random(ALPHA.length)) +
+        ALPHA.charAt(random(ALPHA.length)) +
+        random(BASE) + random(BASE) + random(BASE);
+    if (usedNames[name]) name = generateName();
+    else usedNames[name] = true;
+    return name;
+};
+
+export class Robot {
+    constructor() {
+        this.robotName = generateName();
+    }
+
+    get name() {
+        return this.robotName;
+    }
+
+    reset() {
+        this.robotName = generateName();
+    }
+}
